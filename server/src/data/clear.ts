2b49dc78eaf8385c98aef859ade4729ecb8dd870
orderr66 <kananhagverdiyev@gmail.com>
2021-12-16 14:36:30 +0000

    check connectiity

--- server/src/data/clear.ts
+++ server/src/data/clear.ts
@@ -3,6 +3,11 @@ import { clearNodes } from "./utils";
 
 const startClearing = async (): Promise<any> => {
   const neo4jService: Neo4jService = new Neo4jService();
+  neo4jService.getDefaultDriver().verifyConnectivity().then(result => {
+    console.log("Successfully connected!")
+  }).catch(error=>{
+    process.exit(1)
+  })
   const query: string = `MATCH (n: Node) DETACH DELETE n`
   clearNodes(neo4jService, query).then(result => {
     console.log("Nodes successfully deleted!")

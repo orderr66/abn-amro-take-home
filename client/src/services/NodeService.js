4777b282ab0489938620e23b996d5f6d163ba5c8
orderr66 <kananhagverdiyev@gmail.com>
2021-12-16 03:51:16 +0000

    fix cors

--- client/src/services/NodeService.js
+++ client/src/services/NodeService.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 export default {
   async getNodes() {
-    let res = await axios.get("http://localhost4000/nodes");
+    let res = await axios.get("http://localhost:4000/nodes");
     return res.data;
   },
   async createNode(payload) {
-    let res = await axios.post("http://localhost4000/nodes", payload);
+    let res = await axios.post("http://localhost:4000/nodes", payload);
     return res.data;
   },
   async deleteNode(payload) {
-    let res = await axios.delete("http://localhost4000/nodes", {data: {name: payload.name}});
+    let res = await axios.delete("http://localhost:4000/nodes", {data: {name: payload.name}});
     return res.data;
   }
 }
